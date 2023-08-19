0c7ed3c0a127a37d6b93a80fd53f7842e6d5663e
Aunirbhan Das <aunirbhandas@Aunirbhans-Air.lan>
2023-08-19 00:16:07 -0400

    Added TSX files

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
